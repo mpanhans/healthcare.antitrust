ae65e8682be63da3818b307c08f57279dbd8435b
mpanhans <mpanhans@gmail.com>
2023-12-23 18:14:25 -0600

    remove party_ind input from wtp_calc

--- R/wtp_calc.R
+++ R/wtp_calc.R
@@ -3,7 +3,7 @@
 #' This function calculates the system-level Willingness-To-Pay.
 #'
 #' @param data Dataset of hospital discharges. Required variables:
-#'   \code{cell}, \code{sys_id}, \code{party_ind}, \code{count}, and
+#'   \code{cell}, \code{sys_id}, \code{count}, and
 #'   \code{weight}. Use other function arguments to
 #'   indicate alternative variable names to the defaul names.
 #' @param cell Name of variable specifying cell to which each observation
@@ -11,9 +11,6 @@
 #'   created by \code{cell_defn} function.
 #' @param sys_id Name of variable specifying (numeric) system identifier.
 #'   Default variable name is \code{sys_id}.
-#' @param party_ind Name of indicator variable for whether hospital is a
-#'   party from which diversions should be calculated. Default variable
-#'   name is \code{party_ind}.
 #' @param count Name of variable indicating the number of admissions
 #'   represented by the observation. Set = 1 for every row if each
 #'   observation represents one admission.
@@ -26,8 +23,7 @@
 #'   hospital have the share set to 99.0 percent.
 #'
 #' @returns A data frame with the number of rows equal to the number of systems
-#' in the input data frame, and the following variables: `party` indicates
-#'  whether the system includes party hospitals, `sys_id` is the system
+#' in the input data frame, and the following variables: `sys_id` is the system
 #'  identifier, `WTP_s` is the system's WTP, `N_s` is the number of admission
 #'  for the system, and `WTP_s_wt` is the weighted WTP for the system
 #'  if the weight input is provided.
@@ -55,11 +51,6 @@
 #' outList <- cell_defn(discharge_data,th,layers)
 #' D0 <- outList$assigned
 #'
-#' D0$party_ind <- 0
-#' D0$party_ind[D0$hosp_id==1] <- 1
-#' D0$party_ind[D0$hosp_id==2] <- 1
-#' D0$party_ind[D0$hosp_id==5] <- 1
-#'
 #' out <- wtp_calc(D0)
 #'
 #' @importFrom stats aggregate ave
@@ -73,32 +64,20 @@
 
 # This function calculates the system-level WTP.
 # MAYBE add system id to the output?
-# Also I think party_ind is not really used, remove as input?
 
 wtp_calc <- function(data,
                      cell = "cell",
                      sys_id = "sys_id",
-                     party_ind = "party_ind",
                      count = "count",
                      weight = "weight",
                      dropDegenerateCell = TRUE) {
 
-  ## allow for generic variable names. old. Now [] instead of $
-  #names(data)[names(data) == cell] <- "cell"
-  #names(data)[names(data) == sys_id] <- "sys_id"
-  #names(data)[names(data) == party_ind] <- "party_ind"
-  #names(data)[names(data) == count] <- "count"
-
   ## allow weight to be named or, if missing, set = 1.
-  ## Next two lines were old version, now using [] instead of $
-  #names(data)[names(data) == weight] <- "weight"
-  #if (!"weight" %in% names(data)) { data$weight  <- 1}
   if (! weight %in% names(data)) { data$weight  <- 1}
 
-  #data$totalweight <- data$weight*data$count  # in case some obs are aggregated admissions
   data$totalweight <- data[[weight]]*data[[count]]  # in case some obs are aggregated admissions
 
-  y_cell <- aggregate(list(N_s=data[[count]], wt = data$totalweight),by=list(cell=data[[cell]], sys_id=data[[sys_id]],party=data[[party_ind]]),sum)
+  y_cell <- aggregate(list(N_s=data[[count]], wt = data$totalweight),by=list(cell=data[[cell]], sys_id=data[[sys_id]]),sum)
 
   y_cell <- y_cell[order(y_cell$cell,y_cell$sys_id),]
 
@@ -120,13 +99,7 @@ wtp_calc <- function(data,
   y_cell$WTP_s <- log(1/(1-y_cell$share_s))*y_cell$N
   y_cell$WTP_s_wt <- log(1/(1-y_cell$share_s))*y_cell$N*y_cell$wt
 
-  y <- aggregate(list(WTP_s=y_cell$WTP_s, N_s=y_cell$N_s, WTP_s_wt=y_cell$WTP_s_wt),by=list(party=y_cell$party,sys_id=y_cell$sys_id),sum)
-
-  # Revert variable names
-  #names(y)[names(y) == "cell"] <- cell
-  #names(y)[names(y) == "sys_id"] <- sys_id
-  #names(y)[names(y) == "party_ind"] <- party_ind
-  #names(y)[names(y) == "count"] <- count
+  y <- aggregate(list(WTP_s=y_cell$WTP_s, N_s=y_cell$N_s, WTP_s_wt=y_cell$WTP_s_wt),by=list(sys_id=y_cell$sys_id),sum)
 
   return(y)
 }
